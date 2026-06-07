6f3cff9b5b4cfc1323a8791171270cf00e53af53
cropmarkdsign-commits <cropmarkdsign@gmail.com>
2026-06-06 23:12:51 -0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CuestionarioWGN from "./CuestionarioWGN.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CuestionarioWGN />
+  </React.StrictMode>
+);
